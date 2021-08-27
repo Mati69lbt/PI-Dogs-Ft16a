7c664703fcf5d2a4aec7a1ecd9e5cc77d749cdba
Mati69lbt <Mati69_lbt@hotmail.com>
2021-08-27 19:32:53 -0300

    models and DB

---
+++ api/src/models/Temperament.js
@@ -0,0 +1,10 @@
+const { DataTypes} = require("sequelize");
+
+module.exports = (Sequelize) => {
+  Sequelize.define("temperament", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};
